5899540b843b4c054fe4bb839cb261735ccd1fab
jhon0498 <https://github.com/Jhon0498/Semana4.git>
2024-11-05 10:40:59 +0000

    Atulização

--- forms_templates.py
+++ forms_templates.py
@@ -1,56 +1,63 @@
-# A very simple Flask Hello World app for you to get started with...
 from flask import Flask, request, render_template, session, url_for, redirect, flash
 from datetime import datetime
-
-
 from flask_moment import Moment
-#Parte do WTF
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-
-
-#Parte do WTF
 app.config['SECRET_KEY'] = 'chave forte'
-
 moment = Moment(app)
 
 class NameForm(FlaskForm):
-    name = StringField('Qual teu nome?', validators = [DataRequired()])
-    submit = SubmitField('Submit')
+    name = StringField('Qual teu nome?', validators=[DataRequired()])
+    sobreNome = StringField('Qual teu sobrenome?', validators=[DataRequired()])
+    inst = StringField('Informe a sua Instituição de ensino:', validators=[DataRequired()])
+    disciplina = SelectField('Informe a sua disciplina',
+                             choices=[('DWEBS', 'Desenvolvimento Web: Servidor'),
+                                      ('GSTI', 'Gestão de TI'),
+                                      ('EX4', 'Projeto de Extensão 4')])
+    submit = SubmitField('Enviar')
 
-#Essa rota está para uso do forms
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Mudando nome!')
+        session['navegador'] = request.headers.get('User-Agent')
+        session['Ip_remoto'] = request.headers.get('X-Forwarded-For')
+        session['host_name'] = request.headers.get('Host')
         session['name'] = form.name.data
+        session['sobreNome'] = form.sobreNome.data
+        session['inst'] = form.inst.data
+        session['disciplina'] = form.disciplina.data
         return redirect(url_for('hello_world'))
-    return render_template('formularioTeste.html', form = form, name = session.get('name'))
-"""
-@app.route('/')
-def hello_world():
-    name = "eu estou usando o JINJA2!";
-    return render_template('template-base.html', current_time = datetime.utcnow());
-"""
+    return render_template(
+        'formularioTeste.html',
+        form=form,
+        name=session.get('name'),
+        sobreNome=session.get('sobreNome'),
+        inst=session.get('inst'),
+        disciplina=session.get('disciplina'),
+        browser=session.get('navegador'),
+        ip_remoto=session.get('Ip_remoto'),
+        host_name=session.get('host_name'),
+        current_time=datetime.utcnow()
+    )
+
 @app.route('/user/<name>')
 def hello_pront(name):
-    name2 = name
-    return render_template('user.html', name = name, pront = 'PT3026931', ins = 'IFSP' , current_time = datetime.utcnow())
-
+    return render_template('user.html', name=name, pront='PT3026931', ins='IFSP', current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def not_found(e):
-    return render_template('404.html', current_time = datetime.utcnow()), 404;
+    return render_template('404.html', current_time=datetime.utcnow()), 404
 
 @app.route('/contextorequisicao')
 def hello_requisi_detalhes():
     navegador = request.headers.get('User-Agent')
     Ip_remoto = request.headers.get('X-Forwarded-For')
     host_name = request.headers.get('Host')
-    return render_template('contextorequisicao.html', name = 'Guilherme', navegador = navegador, IP_cliente = Ip_remoto, host_name = host_name);
+    return render_template('contextorequisicao.html', name='Jhonatan', navegador=navegador, IP_cliente=Ip_remoto, host_name=host_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
